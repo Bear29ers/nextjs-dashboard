85509954070adb3fa472eab0af27e37cc53341eb
Yuta Okuma <y.dream.nest@icloud.com>
2023-11-20 21:43:44 +0900

    feature: :sparkles: update the URL with the search params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,10 +1,21 @@
 'use client';
 
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
 const Search = ({ placeholder }: { placeholder: string }) => {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
   const handleSearch = (term: string) => {
-    console.log(term);
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
   };
 
   return (
